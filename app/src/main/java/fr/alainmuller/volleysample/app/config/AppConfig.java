c2c184250a99ea84e3104ac52a13669c0fde6e6d
Alain Muller <alain.muller@niji.fr>
2014-05-15 18:22:48 +0200

    Adding Config file

---
+++ app/src/main/java/fr/alainmuller/volleysample/app/config/AppConfig.java
@@ -0,0 +1,11 @@
+package fr.alainmuller.volleysample.app.config;
+
+/**
+ * Constantes de configuration de l'application (API Keys, ...)
+ * Created by Alain MULLER on 15/05/2014.
+ */
+public class AppConfig {
+
+    /* Weather Underground */
+    public static final String API_KEY = "y0ur0wnApiK3y";
+}
